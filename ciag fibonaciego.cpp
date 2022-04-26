eb114148f52d70ef5e216dca5231cd278eff30e7
bolo420 <100198860+bolo420@users.noreply.github.com>
2022-04-26 10:33:00 +0200

    Add files via upload

---
+++ ciag fibonaciego.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,f0 ,f1 ,f ;
+
+    cout << "Podaj ile chcesz wypisać wyrazów ciągu fibonacciego: ";
+    cin >> n;
+    f0 = 0;
+    f1 = 1;
+    for (int i = 0; i < n; i++)
+    {
+        if (i > 1)
+        {
+
+            f = f0 + f1;
+            f0 = f1;
+            f1 = f;
+        }
+        else
+        {
+            f = i;
+        }
+        cout << f <<endl ;
+    }
+    
+    return 0;
+}
